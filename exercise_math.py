96158428bc3757c8b13fa3e5cd2765737ba6c512
valentinoarzt22 <vrarzt@mail.austral.edu.ar>
2025-03-17 12:04:39 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,11 @@
 def math():
-    a = 57
-    b = 7
+    a = 100
+    b = 5
+# operaciones: la suma, la diferencia, el producto, el promedio, el cociente entero y el resto de la división entera y el valor real de la división. 
+print(a + b)
+print(a - b)
+print(a * b)
+print((a + b) / 2) 
+print(int(a / b))
+print(a % b)
+print(a / b)
