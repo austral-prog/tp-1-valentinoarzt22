ebfc5dfde1fd828d0a88819bb9bf345330c4e735
valentinoarzt22 <vrarzt@mail.austral.edu.ar>
2025-03-17 12:06:41 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,11 +1,11 @@
 def math():
     a = 100
     b = 5
-    
-print(a + b)
-print(a - b)
-print(a * b)
-print((a + b) / 2) 
-print(int(a / b))
-print(a % b)
-print(a / b)
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    print((a + b) / 2) 
+    print(int(a / b))
+    print(a % b)
+    print(a / b)
+
